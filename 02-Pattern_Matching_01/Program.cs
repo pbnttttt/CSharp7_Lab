e12738a84dff12991fef2d8965d586524aa34661
maxchou <callmegto@hotmail.com>
2017-03-27 18:09:17 +0800

    add pattern matching lab

---
+++ 02-Pattern_Matching_01/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+/*
+ * Pattern Matching 稱為模式匹配
+ * 1.是一種新語法，可以用來「測試」值是否為某個型別，是的話還可以進一步取得值的訊息
+ * 
+ * 2.有三種 pattern
+ *   2.1 Constant patterns(常數模式): 以 c 表示( c 為常數). 測試輸入是否等於 c
+ *   2.2 Type patterns(類型模式): 以 T x 表示( T 為型別， x 為以 T 宣告的變數). 測試輸入是否等於 T, 是的話將輸入 assign 給 x 變數.
+ *   2.3 Var patterns(Var 模式): ??
+ *   
+ * 3. pattern matching 強化了現有二種語法結構
+ *   3.1 is-expression (is 運算式). ex: value is pattern
+ *   3.2 switch 的 case 語句
+ *   
+ * 4. 值為 dynamic 型別時，是否可處理
+ *    Constant patterns: 可
+ *    Type patterns: 不可
+ * 
+ * 5. x 稱為 pattern variables，與 out variables 類似，要注意作用域.
+      pattern variables & out variables 可稱為 "expression variables"
+ */
+namespace _02_Pattern_Matching_01
+{
+    /*
+     * Constant patterns(常數模式)
+     */
+    class Program
+    {
+        const string KEY = "ABCD";
+
+        static void Main(string[] args)
+        {
+            //Constant patterns
+            Method01("ABCD");
+
+            //Type patterns
+            Method02(5);
+
+            //可以接受值的型別為 int or string , 但值必需是數字
+            Method03("5");
+            Method03(5);
+        }
+
+        static void Method01(dynamic o)
+        {
+            if(o is KEY) 
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        static void Method02(object o)
+        {
+            if(o is int x)
+            {
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        static void Method03(object o)
+        {
+            if(o is int x || 
+                (o is string s && int.TryParse(s, out x))) //因為 x 在之前的 type patterns 宣告了，在相同作用域下，TryParse 時可以帶入
+            {
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        /// <summary>
+        /// Method03, 的舊寫法，醜!!
+        /// </summary>
+        /// <param name="o"></param>
+        static void Method4(object o)
+        {
+            int x;
+            if (o is int)
+                x = (int)o;
+            else if(o is string && int.TryParse((o as string), out x))
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+
+    class C1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ 02-Pattern_Matching_02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using static System.Console; //C# 6.0
+
+/*
+ * pattern matching 在 switch case 使用上的特色：
+ * 1. 可以 switch 任何型別，不只是原始型別(primitive types)
+ * 2. pattern 可以用在 case 語句
+ * 3. case 語句可以含有額外的條件(when expression)
+ * 
+ * pattern matching 在 switch case 使用上的注意事項：
+ * 1. case 語句的順序是重要的：如果 Rectangle r 放在 Rectangle s 之前，那 Rectangle s 永遠不會被評估到，就會爆錯(error：先前的案例已處理切換案例).
+ * 2. default 子句總是最後被評估：所以 case null 雖然放在最後，但還是會比 default 子句早評估到，但優良的寫法最好把 default 子句放最後
+ * 3. null 子句在最後面並不會無法達到：這是因為類型模式依循目前 is 表達式的例子，而並不匹配 null。這確保 null 值不會不小心被任何類型的模式給搶走；您必須更清楚要如何處理它們（或留它們給 default 子句）。
+ * 4. pattern variables 也要注意作用域
+ */
+namespace _02_Pattern_Matching_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method01("a");
+            //Method01(null);
+            Method01(new Circle { Radius = 3 });
+        }
+
+        static void Method01(object shape)
+        {
+            switch (shape)
+            {
+                case Circle c:
+                    WriteLine($"circle with radius {c.Radius}");
+                    break;
+                case Rectangle s when (s.Length == s.Height):
+                    WriteLine($"{s.Length} x {s.Height} square");
+                    break;
+                case Rectangle r:
+                    WriteLine($"{r.Length} x {r.Height} rectangle");
+                    break;
+                default:
+                    WriteLine("<unknown shape>");
+                    break;
+                case null: //constant pattern
+                    throw new ArgumentNullException(nameof(shape));
+            }
+        }
+    }
+
+    class Circle
+    {
+        public int Radius { get; set; }
+    }
+
+    class Rectangle
+    {
+        public int Length { get; set; }
+        public int Height { get; set; }
+    }
+}
